2748604a4f74834d1d86f480a09c24805c9cfd2c
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-02 14:11:02 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<template>
+  <LayoutHeader />
+</template>
+
+<script>
+import LayoutHeader from "@/components/LayoutHeader.vue";
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+  },
+};
+</script>
+
 <style>
 @import url("./reset.css");
 </style>
