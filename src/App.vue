e593d556be4187d17c1b12d4f63bef8c5cfc954d
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-02 14:16:49 +0200

    feat: add book list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <LayoutHeader />
+  <BookList />
 </template>
 
 <script>
 import LayoutHeader from "@/components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
 export default {
   name: "App",
   components: {
     LayoutHeader,
+    BookList,
   },
 };
 </script>
