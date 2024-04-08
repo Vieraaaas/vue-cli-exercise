347f7b3319fe39e6cb38af9a381782572bf6532f
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-08 13:52:55 +0200

    fix: delete placeholder views

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <LayoutHeader />
+  <BookList />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import LayoutHeader from "@/components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
