25936f1ac18400f4acea6eb8d533f28ec9d47a35
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-08 10:20:48 +0200

    chore: install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <LayoutHeader />
-  <BookList />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import LayoutHeader from "@/components/LayoutHeader.vue";
-import BookList from "@/components/BookList.vue";
-export default {
-  name: "App",
-  components: {
-    LayoutHeader,
-    BookList,
-  },
-};
-</script>
-
 <style>
-@import url("./reset.css");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
